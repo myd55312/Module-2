04367db4de683865ef608aba11a97c930f28ee9f
myd55312 <myd55312@gmail.com>
2024-01-18 09:13:41 +0700

    bai 3

---
+++ Array.java
@@ -0,0 +1,33 @@
+public class Array {
+    public static void main(String[] args) {
+    }
+}
+//    public static void main(String[] args) {
+//        int[] arr = {1,2,3,4,5,6};
+//        int a = 11;
+//        int index = 3 ;
+
+        //them 11 vao cuoi mang
+//        int[] newArr = new int[arr.length + 1 ];
+//        for (int i = 0; i < arr.length; i++) {
+//            newArr[i] = arr[i];
+//        }
+//        newArr[newArr.length-1] = a;
+//                arr = newArr ;
+//        System.out.println(Arrays.toString(arr));
+
+        // them11 vao giua mang
+//        int[] newArr = new int [arr.length + 1];
+//        newArr[index] = a ;
+//        for (int i = 0; i < index; i++) {
+//            newArr[i] = arr[i];
+//        }
+//        System.out.println(Arrays.toString(newArr));
+//        for (int i =index+1 ; i <newArr.length ; i++) {
+//            newArr[i] = arr[i-1];
+//        }
+//        System.out.println(Arrays.toString(newArr));
+//    }
+//}
+        //xoa phan tu tai vi tri 3
+
